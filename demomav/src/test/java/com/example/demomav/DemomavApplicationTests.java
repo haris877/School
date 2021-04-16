e3c1a31894e67dd90a40bb676fe214d1691cb31a
haris877 <81170462+haris877@users.noreply.github.com>
2021-04-16 13:53:43 +0200

    hej hej

---
+++ demomav/src/test/java/com/example/demomav/DemomavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomavApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
